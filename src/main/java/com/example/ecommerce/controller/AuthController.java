5dc68fc4ca74bc15e81f6e7f2a5233ad8cdc19f1
YouHyeunWoo <dbgusdn7854@naver.com>
2023-12-18 15:44:25 +0900

    no message

---
+++ src/main/java/com/example/ecommerce/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+@Slf4j
+public class AuthController {
+
+}
