a378af893766a0ec393f27ba4dc73e0770ef8d53
YouHyeunWoo <dbgusdn7854@naver.com>
2023-12-19 17:36:16 +0900

    feat : 회원가입 API구현 (Spring Security의 PasswordEncoder, FilterChain) feat : 로그인 시 발행될 JWT토큰 생성 기능 구현, 토큰의 유효성 검사 기능 구현

--- src/main/java/com/example/ecommerce/controller/AuthController.java
+++ src/main/java/com/example/ecommerce/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.domain.MemberEntity;
+import com.example.ecommerce.model.Auth;
+import com.example.ecommerce.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @Slf4j
 public class AuthController {
+    private final MemberService memberService;
 
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Auth.Register request){
+        MemberEntity memberEntity = this.memberService.register(request);
+
+        return ResponseEntity.ok(memberEntity);
+    }
 }
