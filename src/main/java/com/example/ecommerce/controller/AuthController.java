649444df90ebf6db924440ff37e7f9bb57d368be
YouHyeunWoo <dbgusdn7854@naver.com>
2023-12-19 20:22:27 +0900

    feat : controller 패키지에 lonIn API추가

--- src/main/java/com/example/ecommerce/controller/AuthController.java
+++ src/main/java/com/example/ecommerce/controller/AuthController.java
@@ -6,10 +6,7 @@ import com.example.ecommerce.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,5 +20,11 @@ public class AuthController {
         MemberEntity memberEntity = this.memberService.register(request);
 
         return ResponseEntity.ok(memberEntity);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> logIn(){
+
+        return null;
     }
 }
